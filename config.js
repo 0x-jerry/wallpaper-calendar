a8a943feee2ec204a5bc8c1f5c6bdcca09e7a37b
Fantasy <jie844067636@gmail.com>
2019-09-16 23:32:39 +0800

    perf: Update config

--- config.js
+++ config.js
@@ -1,14 +1,10 @@
 const _config_ = {
   weather: {
+    // https://www.seniverse.com
     key: 'PtQpA4GDySVurAZXo',
     secret: 'SfqiClugXoaw-O_tG',
-    // Latitude: longitude
     city: 'ip',
     // seconds
     updateRate: 30 * 60
-  },
-  calendar: {
-    rx: 0,
-    ry: 0
   }
 }
